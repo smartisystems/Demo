f32330f1883b8a6af2ba94b5a3be9358927156c8
Administrator <smartisystemspune@gmail.com>
2017-10-10 11:00:02 +0530

    first commint

---
+++ app/src/main/java/com/vbm/application/SpalshActivity.java
@@ -0,0 +1,18 @@
+package com.vbm.application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SpalshActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spalsh);
+    }
+
+
+
+}
